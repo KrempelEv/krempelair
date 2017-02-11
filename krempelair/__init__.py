8be894f7e86f1d35b0fb45c7fb7d2fa2e2c9bb95
graphics80 <K.Ilkow@gmail.com>
2017-02-11 11:30:02 +0100

    rename status to stufe

--- krempelair/__init__.py
+++ krempelair/__init__.py
@@ -42,7 +42,7 @@ class Krempelair(flask.Flask):
             ('air_get_status_betrieb',   '/'),
             ('air_get_status_stoerung',   '/stoerung'),
             ('air_set_status',   '/<int:pin>/<int:state>'),
-            ('air_set_level', '/lueftung/status/<int:level>'),
+            ('air_set_level', '/lueftung/stufe/<int:level>'),
             ('air_set_timer', '/lueftung/timer/<int:time>'),
             ('air_set_temp', '/lueftung/temperatur/<int:temp>'),
             ('air_set_raucherraum_on', '/raucherraum/on'),
