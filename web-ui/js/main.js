73104bfbef721dd7ff68a05f79137b9d0938273c
graphics80 <K.Ilkow@gmail.com>
2017-03-13 21:48:16 +0100

    Update main.js

--- web-ui/js/main.js
+++ web-ui/js/main.js
@@ -1,6 +1,98 @@
-
-var KrempelAirApp = angular.module('KrempelAirApp', []);
+var KrempelAirApp = angular.module('KrempelAirApp', ['ngRoute']);
 var uri = "/api"
+
+KrempelAirApp.config(['$locationProvider', function($locationProvider) {
+  $locationProvider.hashPrefix('');
+}]);
+
+
+KrempelAirApp.config(function ($routeProvider) {
+  $routeProvider
+      .when('/', {
+        templateUrl: '../views/air.html',
+        controller: 'MainCtrl'
+      })
+      .when('/login', {
+        //template: '<form ng-submit="login()" name="loginForm"> username: <input type="text" ng-model="user.username"> <br/> password: <input type="password" ng-model="user.password"><button>login</button></form>',
+        templateUrl: '../views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+})
+
+KrempelAirApp.factory('Auth', function ($rootScope, $window, $http) {
+  return {
+    login: function (user, successHandler, errorHandler) {
+      if (user.username == 'test' && user.password == 'test') {
+        this.setLoggedInUser(user);
+        successHandler(user);
+      } else {
+        alert("please use username/password as test/test to login");
+        errorHandler(user);
+      }
+
+      // call the server side login restful api
+//      $http.post('api/login', user).success(function(user) {
+//        this.setLoggedInUser(user);
+//        successHandler(user);
+//      }).error(errorHandler);
+    },
+    getLoggedInUser: function () {
+      if ($rootScope.user === undefined || $rootScope.user == null) {
+        var userStr = $window.sessionStorage.getItem('user');
+        if (userStr) {
+          $rootScope.user = angular.fromJson(userStr);
+        }
+      }
+      return $rootScope.user;
+    },
+    isLoggedIn: function () {
+      return this.getLoggedInUser() != null;
+    },
+    setLoggedInUser: function (user) {
+      $rootScope.user = user;
+      if (user == null) {
+        $window.sessionStorage.removeItem('user');
+      } else {
+        $window.sessionStorage.setItem('user', angular.toJson($rootScope.user));
+      }
+    }
+  };
+})
+
+KrempelAirApp.controller('LoginCtrl', function ($scope, Auth, $location, $http) {
+  $scope.user = {};
+
+  $scope.login = function () {
+    Auth.login($scope.user, function () {
+      $location.path('/');
+    }, function (e) {
+      // do some error handling.
+    });
+  };
+})
+
+KrempelAirApp.controller('MainCtrl', function ($scope, Auth, $location) {
+
+  $scope.logout = function () {
+    Auth.setLoggedInUser(null);
+    $location.path('/somewhere');
+  };
+})
+
+KrempelAirApp.run(['$window', '$rootScope', '$location', 'Auth', function ($window, $rootScope, $location, Auth) {
+
+    $rootScope.$on("$routeChangeStart", function (event) {
+      if (!Auth.isLoggedIn() &&
+          $location.path() !== '/login') {
+        $location.path('/login');
+      }
+    });
+}]);
+
+
 KrempelAirApp.controller('AirFlowController', function AirFlowController($scope, $http, $timeout) {
 
 
@@ -10,7 +102,7 @@ KrempelAirApp.controller('AirFlowController', function AirFlowController($scope,
         });
         $http.get(uri+"/stoerung").then(function (response) {
             $scope.stoerung = response.data;
-        }); 
+        });
         $http.get(uri+"/lueftung/temperatur").then(function (response) {
             $scope.temperatur = response.data;
             if($scope.sollTemp && (parseFloat(parseFloat($scope.sollTemp).toFixed(2)) !== parseFloat(parseFloat(response.data["TempSoll"]).toFixed(2)))){
@@ -24,8 +116,8 @@ KrempelAirApp.controller('AirFlowController', function AirFlowController($scope,
                 $scope.sollTempNAK = response.data["TempSollNAK"];
             }
         });
-       
-        
+
+
         $timeout(function(){
          $scope.Refresh();
         },800);
@@ -34,6 +126,6 @@ KrempelAirApp.controller('AirFlowController', function AirFlowController($scope,
         $http.get(uri+"/lueftung/stufe/"+Klick);
         $scope.Refresh();
     }
-    
+
     $scope.Refresh();
 });
