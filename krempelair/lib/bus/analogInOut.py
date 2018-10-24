e375a8e669c2e2d0739f6d63f024477d6f8a40d4
graphics80 <K.Ilkow@gmail.com>
2018-10-24 21:38:49 +0200

    Update analogInOut.py

--- krempelair/lib/bus/analogInOut.py
+++ krempelair/lib/bus/analogInOut.py
@@ -10,11 +10,27 @@ class analogInOut():
     def __init__(self):
         self._bus = smbus.SMBus(1)
 
-    def getValue(self, address, chanel):
+    def getValueOld(self, address, chanel):
         var = self._bus.read_i2c_block_data(address,chanel,11)    #Werte von Board in 11 stelliges Array schreiben
         val = var[2]*256+var[3]                                   #Berechnung der korrekten Zahlenwerte aus dem Array
         log.debug("Analogwert von Adresse "+str(address)+ " mit Kanal " +str(chanel) +" mit Wert "+ str(val))    #Ausgabe in der Python Shell
         return val
+    
+    
+    def getValue(self, address, chanel):
+        var = self._bus.read_i2c_block_data(address,0x00,11)    #Werte von Board in 11 stelliges Array schreiben
+        if chanel == 0:
+            val = var[2]*256+var[1]
+        if chanel == 1:
+            val = var[4]*256+var[3]                                   #Berechnung der korrekten Zahlenwerte aus d$
+        if chanel == 2:
+            val = var[6]*256+var[5]
+        if chanel == 3:
+            val = var[8]*256+var[7]
+        if chanel == 4:
+            val = var[10]*256+var[9]
+        return val
+
 
     def setValue(self, address, chanel, value):
         a=int(value)
